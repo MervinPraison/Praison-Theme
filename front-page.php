ee813a3ac8fef73325a87989ea32bc755f0c4864
mervinpraison <mervin@aagplc.com>
2017-12-21 12:35:00 +0000

    'front-page1'

--- front-page.php
+++ front-page.php
@@ -1,4 +1,23 @@
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
+<div class="fp-top container">
+	<div class="row p-3">
+		<div class="col-md-6">
+			<?php get_search_form(); ?>
+		</div>
+		<div class="col-md-6">			
+		</div>
+		
+	</div>
+	
+	
+</div>
+<div class="fp-middle container">
+	<div class="row p-1">		
+	</div>
+</div>
+ 
+
+ <?php while (have_posts()) : the_post(); ?>
+  <?php //get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/content', 'page'); ?>
+  
 <?php endwhile; ?>
