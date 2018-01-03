ab7289b18037766888cdf42e7a7caa67d04cbeca
MervinPraison <mervin@aagplc.com>
2018-01-03 11:02:48 +0000

    'navbar-update-only'

--- templates/header.php
+++ templates/header.php
@@ -1,8 +1,10 @@
 <header class="banner">
-  <div class="container clearfix">
-    <a class="brand align-text-bottom" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-end">
+  <div class="container">
+   	<!--<div class="row">-->
+    <!--<a class="brand align-text-bottom" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>-->
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark justify-content-end fixed-top">
     <!--<span class="navbar-brand">Menu</span>-->
+    <a class="brand navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -24,5 +26,6 @@
       ?>
       </div>
     </nav>
+	<!--</div>-->
   </div>
 </header>
