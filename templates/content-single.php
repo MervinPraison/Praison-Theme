00ab0f2b21edad1eae4782fcd2d125d25c55f270
MervinPraison <mervin@aagplc.com>
2018-01-04 16:03:29 +0000

    'SocialMedia-Share'

--- templates/content-single.php
+++ templates/content-single.php
@@ -5,16 +5,41 @@
   <article <?php post_class(); ?>>
     <header>
       <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
+      <div class="container">
+      	<div class="row">      	
+      		<div class="col-md-8 nopadding">
+      			<?php get_template_part('templates/entry-meta'); ?>			
+      		</div>
+      		<div class="col-md-4 nopadding">
+      			<div class="socialmedia-share pb-3">
+				Share on 
+					<?php
+					$socialURL = urlencode(get_permalink());
+
+					$socialTitle = str_replace( ' ', '%20', get_the_title());
+
+					//$socialThumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
+
+					$twitterURL = 'https://twitter.com/intent/tweet?text='.$socialTitle.'&amp;url='.$socialURL;
+					$facebookURL = 'https://www.facebook.com/sharer/sharer.php?u='.$socialURL;
+
+					echo '<a href="'.$twitterURL.'" target="_blank"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>&nbsp;&nbsp;';
+
+					echo '<a href="'.$facebookURL.'" target="_blank"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a>';
+					?>
+				</div>
+      		</div>
+      	</div>      	
+      </div>
+      
     </header>
     <div class="entry-content">
       <?php the_content(); ?>
-    </div>
+    </div>    
+    <br />
     <footer>
       <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
     </footer>
     <?php comments_template('/templates/comments.php'); ?>
   </article>
 <?php endwhile; ?>
-
-
