ca5d70886f015682308058910f2b670968d0e280
MervinPraison <mervin@aagplc.com>
2018-01-04 11:12:56 +0000

    'breadcrumb-2'

--- lib/wp-bootstrap-breadcrumb.php
+++ lib/wp-bootstrap-breadcrumb.php
@@ -8,6 +8,13 @@ function wps_mervin_breadcrumb_bootstrap() {
             false
         );
         echo str_replace( '»', '</li><li>', $breadcrumb );
+    }elseif ( function_exists( 'yoast_breadcrumb' ) ) {
+        $breadcrumb = yoast_breadcrumb(
+            '<ul class="breadcrumb"><li>',
+            '</li></ul>',
+            false
+        );
+        echo str_replace( '»', '</li><li>', $breadcrumb );
     }
 }
 
