3b4d39e78cb79938bf9908b3583e7ae09f093653
MervinPraison <praison.ms@gmail.com>
2018-01-15 12:26:35 +0000

    'Front-page-content-added'

--- front-page.php
+++ front-page.php
@@ -13,12 +13,17 @@
 	<div class="row p-1">		
 	</div>
 </div>-->
-
-<?php while (have_posts()) : the_post(); ?>
-  <?php //get_template_part('templates/page', 'header'); ?>
-  <?php //get_template_part('templates/content', 'page'); ?>
-  
-<?php endwhile; ?>
+<div class="container ">
+	<div class="row pt-5 px-3">
+		<div class="px-3">
+		<?php while (have_posts()) : the_post(); ?>
+		  <?php //get_template_part('templates/page', 'header'); ?>
+		  <?php get_template_part('templates/content', 'page'); ?>
+		  
+		<?php endwhile; ?>
+		</div>
+	</div>
+</div>
 
 <?php //get_template_part('templates/page', 'header'); ?>
 <?php 
