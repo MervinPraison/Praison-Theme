48f50810290203ce3b37b91e03c3c2699da7eee8
MervinPraison <praison.ms@gmail.com>
2018-01-18 16:54:38 +0000

    'fp-block-fix'

--- templates/front-page-footer.php
+++ templates/front-page-footer.php
@@ -1,6 +1,8 @@
+<?php if ( is_active_sidebar( 'mainbar-home' ) ) {  ?>
 <footer class="content-info">
 	<br /><br /><br />
 </footer>
+<?php } ?>
 <div class="container ">
 	<div class="row p-3">
 		<div class="col-md-8">
